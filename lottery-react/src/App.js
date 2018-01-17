e09545f66bf453e3bacb2cfe727b50d103e37cab
Ana Liza Pandac <analizapandac@gmail.com>
2018-01-17 22:22:41 +0800

    lottery-react: added enter and pickWinner functionalities

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
 	state = {
 		manager: '',
 		players: [],
-		balance: ''
+		balance: '',
+		value: '',
+		message: ''
 	};
 
 	async componentDidMount() {
@@ -20,12 +22,57 @@ class App extends Component {
 		this.setState({ manager, players, balance });
 	}
 
+	onSubmit = async (event) => {
+		// value of 'this' is equal to the function current context
+		event.preventDefault();
+
+		const accounts = await web3.eth.getAccounts();
+
+		this.setState({ message: 'Waiting on transaction success...' });
+
+		await lottery.methods.enter().send({
+			from: accounts[0],
+			value: web3.utils.toWei(this.state.value, 'ether')
+		});
+
+		this.setState({ message: 'You have entered the lottery' });
+	}
+
+	onClick = async () => {
+		const accounts = await web3.eth.getAccounts();
+
+		this.setState({ message: 'Waiting on transaction success...' });
+
+		await lottery.methods.pickWinner().send({
+			from: accounts[0]
+		});
+
+		this.setState({ message: 'A winner has been picked.' });
+	}
+
 	render() {
 		return (
 			<div>
 				<h2>Lottery Contract</h2>
 				<p>This contract is managed by {this.state.manager}</p>
 				<p>There are currently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether.</p>
+				<hr />
+				<form onSubmit={this.onSubmit}>
+					<h4>Want to try your luck?</h4>
+					<div>
+						<label>Amount of ether to enter</label>
+						<input 
+							value={this.state.value}
+							onChange={event => this.setState({ value: event.target.value })}
+						/>
+					</div>
+					<button>Enter</button>
+				</form>
+				<hr />
+				<h4>Ready to pick a winner?</h4>
+				<button onClick={this.onClick}>Pick a winner!</button>
+				<hr />
+				<h1>{this.state.message}</h1>
 			</div>
 		);
 	}
