91c169df087e627bb02e490f479c8758820841cc
Ana Liza Pandac <analizapandac@gmail.com>
2018-01-18 23:54:11 +0800

    kickstart: initial test setup

---
+++ kickstart/test/Campaign.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+	accounts = await web3.eth.getAccounts();
+
+	factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+					.deploy({ data: compiledFactory.bytecode })
+					.send({ from: accounts[0], gas: '1000000' });
+});
