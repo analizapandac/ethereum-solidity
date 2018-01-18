f90a028a68e322d704f6d03f0988656ec33006f4
Ana Liza Pandac <analizapandac@gmail.com>
2018-01-18 23:42:58 +0800

    kickstart: added improved compile script for contracts

--- kickstart/ethereum/compile.js
+++ kickstart/ethereum/compile.js
@@ -1,1 +1,19 @@
-compile.js
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath); // checks to see if directory exists
+
+for (let contract in output) {
+	fs.outputJsonSync(
+		path.resolve(buildPath, contract.replace(':', '') + '.json'),
+		output[contract]
+	);
+}
