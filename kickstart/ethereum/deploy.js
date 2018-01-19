b5915c495293ef6d537bea89877df72cc914b0cf
Ana Liza Pandac <analizapandac@gmail.com>
2018-01-19 17:22:04 +0800

    kickstart: modified deploy.js to fit new contract compilation process

--- kickstart/ethereum/deploy.js
+++ kickstart/ethereum/deploy.js
@@ -1,1 +1,25 @@
-deploy.js
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+	'caught morning convince basket thank devote wisdom budget ship settle bean cycle',
+	'https://rinkeby.infura.io/rS25WLxbN4BWYdTdxVLO'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts();
+
+	console.log('Attempting to deploy from account', accounts[0]);
+
+	const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+		.deploy({ data: compiledFactory.bytecode })
+		.send({ from: accounts[0], gas: '1000000' });
+
+	
+	console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
