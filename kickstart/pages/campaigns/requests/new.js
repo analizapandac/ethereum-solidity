b1e3d040fea536bf4c6a83354891ce901c7134d8
Ana Liza Pandac <analizapandac@gmail.com>
2018-01-22 15:54:25 +0800

    kickstart: finished 'create new request' functionality

--- kickstart/pages/campaigns/requests/new.js
+++ kickstart/pages/campaigns/requests/new.js
@@ -9,7 +9,9 @@ class RequestNew extends Component {
 	state = {
 		value: '',
 		description: '',
-		recipient: ''
+		recipient: '',
+		errorMessage: '',
+		loading: false
 	};
 
 	static getInitialProps(props) {
@@ -18,11 +20,37 @@ class RequestNew extends Component {
 		return { address };
 	}
 
+	onSubmit = async event => {
+		event.preventDefault();
+
+		this.setState({ loading: true, errorMessage: '' });
+
+		const campaign = await Campaign(this.props.address);
+		const { description, value, recipient } = this.state;
+
+		try {
+			const accounts = await web3.eth.getAccounts();
+
+			await campaign.methods.createRequest(description, web3.utils.toWei(value, 'ether'), recipient).send({
+				from: accounts[0]
+			});
+
+			Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+		} catch (err) {
+			this.setState({ errorMessage: err.message });
+		}
+
+		this.setState({ loading: false });
+	};
+
 	render() {
 		return (
 			<Layout>
+				<Link route={`/campaigns/${this.props.address}/requests`}>
+					<a>Back</a>
+				</Link>
 				<h3>Create a Request</h3>
-				<Form>
+				<Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
 					<Form.Field>
 						<label>Description</label>
 						<Input
@@ -46,6 +74,9 @@ class RequestNew extends Component {
 							onChange={ event => this.setState({ recipient: event.target.value }) }
 						/>
 					</Form.Field>
+
+					<Message error header="Oops!" content={this.state.errorMessage} />
+					<Button primary loading={this.state.loading}>Create</Button>
 				</Form>
 			</Layout>
 		);
